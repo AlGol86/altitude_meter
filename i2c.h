2447218114c0c8c250852c7ae76f86e4c4732256
AlGol86 <59099639+AlGol86@users.noreply.github.com>
2020-03-27 13:20:21 +0300

    Create i2c.h

---
+++ i2c.h
@@ -0,0 +1,23 @@
+//this example is intended for 2 MHz clock frequency
+#include "iostm8s103f3.h"
+
+#define address_iic_oled 0x3c
+//iic
+#define PIN_sda     4   //port B
+#define PIN_clk     5   //port B
+
+#define BIT_sda    (1<<PIN_sda)
+#define BIT_clk    (1<<PIN_clk)
+#define BIT_read    1
+#define READ        1
+#define BIT_write   0
+#define WRITE       0
+#define ACK         0 //acknowledge
+#define NOT_ACK     1 //not acknowledge
+
+void sys_del_us(char del_us);
+char start_iic (char adr_iic, char read_write_bit);
+char send_byte (char data_byte);
+unsigned char receive_byte (char acknowledge);
+void stop_iic (void);
+char get_addresses_iic( char n );
